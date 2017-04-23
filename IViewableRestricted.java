b51136326f0ef8ae0a7efa32080507d09c393426
anddopphoopha <andrew.dopphoopha@gmail.com>
2017-04-24 06:59:59 +0700

    Delete IViewableRestricted.java

--- IViewableRestricted.java
+++
@@ -1,8 +0,0 @@
-package clinic;
-
-import java.lang.reflect.InvocationTargetException;
-
-public interface IViewableRestricted {
-	public void viewAppointment(String person) throws IllegalAccessException, IllegalArgumentException,
-			InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException;
-}
